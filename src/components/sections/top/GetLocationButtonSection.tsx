ee269c71cba41d63c739a07ce22e482eb8c145e3
oonuma <oonuma@crew-m.com>
2025-06-03 03:25:42 +0900

    ボタンの距離を縮める

--- src/components/sections/top/GetLocationButtonSection.tsx
+++ src/components/sections/top/GetLocationButtonSection.tsx
@@ -4,7 +4,7 @@ import GetLocationButton from '@/components/common/GetLocationButton';
 
 export default function LocationSortSection() {
   return (
-    <section className="w-full bg-white py-6">
+    <section className="w-full bg-white py-1">
       <div className="max-w-[1200px] mx-auto px-4">
         <div className="flex justify-center">
           <GetLocationButton />
