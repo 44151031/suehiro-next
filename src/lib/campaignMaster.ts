e0063a2fe7c11d77384acea65253add88ad67a99
oonuma <oonuma@crew-m.com>
2025-07-16 00:03:15 +0900

    追加

--- src/lib/campaignMaster.ts
+++ src/lib/campaignMaster.ts
@@ -5,5 +5,6 @@ import { campaignsC } from "./campaignMasterC";
 import { campaignsD } from "./campaignMasterD";
 import { campaignsE } from "./campaignMasterE";
 import { campaignsF } from "./campaignMasterF";
+import { campaignsG } from "./campaignMasterG";
 
-export const campaigns = [...campaignsA, ...campaignsB, ...campaignsC, ...campaignsD, ...campaignsE, ...campaignsF];
+export const campaigns = [...campaignsA, ...campaignsB, ...campaignsC, ...campaignsD, ...campaignsE, ...campaignsF, ...campaignsG];
